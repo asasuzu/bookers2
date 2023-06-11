e95db91b49246853edcfe674a93b4d05b162a40c
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-06-11 13:05:41 +0000

    0611

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,14 @@ class ApplicationController < ActionController::Base
 # configure_permitted_parametersメソッドが実行される。
  before_action :configure_permitted_parameters, if: :devise_controller?
   
+  def after_sign_in_path_for(resource)
+   user_path(current_user.id)
+  end
+  
   protected #protectedは呼び出された他のコントローラからも参照することができる。
   
-  # configure_permitted_parametersメソッドでは、
-  # devise_parameter_sanitizer.permitメソッドを使うことで
-  # ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可している。
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 end
 
