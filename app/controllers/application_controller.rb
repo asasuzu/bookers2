8ce51442e267c6ceac16eb68cd69339790f6e033
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-06-13 15:49:08 +0000

    0613

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
- 
-# devise利用の機能（ユーザ登録、ログイン認証など）が使われる前に
-# configure_permitted_parametersメソッドが実行される。
- before_action :configure_permitted_parameters, if: :devise_controller?
-  
+  before_action :authenticate_user!, except: [:top, :about]
+  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前に
+  # configure_permitted_parametersメソッドが実行される。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
    user_path(current_user.id)
   end
-  
+
   protected #protectedは呼び出された他のコントローラからも参照することができる。
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_userbook, only: [:edit, :update, :destroy]
+  
   def index
     @books = Book.all
     @user = current_user
@@ -8,14 +10,20 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
+    if @book.save
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
   
   def show
     @book = Book.find(params[:id])
-    @user = current_user
     @new_book = Book.new
+    @user = Book.find(params[:id]).user
   end
 
   def edit
@@ -23,9 +31,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
@@ -40,4 +52,10 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :body)
   end
 
+  def is_matching_login_userbook
+    @books = current_user.books
+    @book = @books.find_by(id: params[:id])
+    redirect_to books_path unless @book
+  end
+  
 end
