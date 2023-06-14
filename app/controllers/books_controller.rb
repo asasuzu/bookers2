130945cf33929fe8a60e70662227f49ed02ec7d1
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-06-14 01:27:57 +0000

    submit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :is_matching_login_userbook, only: [:edit, :update, :destroy]
-  
+
   def index
     @books = Book.all
     @user = current_user
@@ -19,7 +19,7 @@ class BooksController < ApplicationController
       render :index
     end
   end
-  
+
   def show
     @book = Book.find(params[:id])
     @new_book = Book.new
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -39,23 +39,24 @@ class BooksController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    redirect_to books_path
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
 
   def is_matching_login_userbook
-    @books = current_user.books
-    @book = @books.find_by(id: params[:id])
-    redirect_to books_path unless @book
+    book = Book.find(params[:id])
+    unless book.user == current_user
+    redirect_to books_path
+    end
   end
-  
+
 end
